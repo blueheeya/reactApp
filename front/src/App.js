8f531eef83e24e5291e159d644a54118fd57e678
blueheeya83 <blueheeya12@gmail.com>
2024-04-16 23:30:59 +0900

    2024.topmenu

--- front/src/App.js
+++ front/src/App.js
@@ -1,31 +1,31 @@
 import React from "react";
 // import {useState,useEffect} from 'react'
-import {Routes, Route} from 'react-router-dom'
-import './assets/css/tailStyle.scss'
-import './assets/css/style.scss'
+import { Route, Routes } from 'react-router-dom';
+import './assets/css/style.scss';
+import './assets/css/tailStyle.scss';
 import Header from "./layout/Header";
-import Home from "./page/Home";
-import Swiper from "./page/Swiper/SwiperPage";
-import Pagenation from "./page/Pager/Pagenation";
-import RegisterPage from "./page/Register/RegisterPage";
-import LoginPage from "./page/Login/LoginPage";
-import ReduxPage from "./page/Redux/ReduxPage";
-import TodoList from "./page/Pager/TodoList";
 import ApiKakao from "./page/API/ApiKakao";
+import Home from "./page/Home";
+import LoginPage from "./page/Login/LoginPage";
+import Pagenation from "./page/Pager/Pagenation";
+import TodoList from "./page/Pager/TodoList";
+import ReduxPage from "./page/Redux/ReduxPage";
+import RegisterPage from "./page/Register/RegisterPage";
+import Swiper from "./page/Swiper/SwiperPage";
 function App() {
   return (
     <>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/swiper" element={<Swiper />}></Route>
-      <Route path="/pager" element={<Pagenation />}></Route>
-      <Route path="/todo" element={<TodoList />}></Route>
-      <Route path="/redux" element={<ReduxPage />}></Route>
-      <Route path="/api" element={<ApiKakao />}></Route>
-      <Route path="/login" element={<LoginPage />}></Route>
-      <Route path="/regist" exact element={<RegisterPage />}></Route>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/swiper" element={<Swiper />}></Route>
+        <Route path="/pager" element={<Pagenation />}></Route>
+        <Route path="/todo" element={<TodoList />}></Route>
+        <Route path="/redux" element={<ReduxPage />}></Route>
+        <Route path="/api" element={<ApiKakao />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/regist" exact element={<RegisterPage />}></Route>
+      </Routes>
     </>
   );
 }
